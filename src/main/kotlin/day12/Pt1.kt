700b012cc9333e31431d0b8cc26dabb85dda0a72
Richard Tran <richard.p.tran@gmail.com>
2023-03-08 23:53:57 -0500

    Checkpoint commit for day12

--- src/main/kotlin/day12/Pt1.kt
+++ src/main/kotlin/day12/Pt1.kt
@@ -1,7 +1,40 @@
 package day12
 
+import java.util.LinkedList
+
 fun main() {
 
-    {}.javaClass.getResourceAsStream("/day3/input.txt")?.bufferedReader()?.readLines()
+    val grid: List<List<Char>> = {}.javaClass.getResourceAsStream("/day12/input.txt")?.bufferedReader()?.readLines()
+        ?.map {
+            it.toCharArray().toList()
+        }.orEmpty()
 
+    bfs(grid)
 }
+
+fun bfs(grid: List<List<Char>>) {
+    val queue: LinkedList<Vertex> = LinkedList()
+    val startingElevation = 'a'
+    var startX = -1
+    var startY = -1
+
+    // Find S
+    grid.forEachIndexed { x, line ->
+        line.forEachIndexed { y, ch ->
+            if (ch == 'S') {
+                println(ch)
+                startX = x
+                startY = y
+            }
+        }
+    }
+    println(startX)
+    println(startY)
+}
+
+data class Vertex(
+    val x: Int,
+    val y: Int,
+    val distance: Int,
+    val character: Char // elevation
+)
